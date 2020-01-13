7fd119c551b287a80d1257e7bd14f8ea0c7ae779
AMcCartneyK3 <59688905+AMcCartneyK3@users.noreply.github.com>
2020-01-13 14:39:42 +0000

    Added basic node server

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var app = express();
+var router = express.Router();
 
-app.get('/',function(req,res) {
-  res.send("Hello K3!");
+router.get('/',function(req,res) {
+    res.send("Hello K3!");
 });
 
-app.listen(4000);
+app.use('/',router);
+
+app.listen(3000);
